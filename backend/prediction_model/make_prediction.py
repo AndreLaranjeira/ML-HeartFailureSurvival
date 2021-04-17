1f2bbcffe68408e20c19ea4e0a2a19bfe5a9bf06
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-04-17 00:18:34 -0300

    Backend - Job to process patient predictions * Created structure for jobs declaration in project; * Created job to process all patient predictions that are currently waiting a result; * Created a python script to make a prediction of heart failure with a machine learning model; * Installed packages node-schedule and python-bridge; * Updated .gitignore to include pycache from the python script used to make predictions;

---
+++ backend/prediction_model/make_prediction.py
@@ -0,0 +1,45 @@
+# Module to make a prediction with a machine learning model.
+
+# Package imports.
+import pandas as pd
+import pickle
+
+
+# Make prediction function.
+def make_prediction(
+    model_filename,
+    age,
+    anaemia,
+    creatinine_phosphokinase,
+    diabetes,
+    ejection_fraction,
+    high_blood_pressure,
+    platelets,
+    serum_creatinine,
+    serum_sodium,
+    sex,
+    smoking,
+    time
+):
+
+    # Prediction data.
+    prediction_data = pd.DataFrame(data={
+        'age': age,
+        'anaemia': anaemia,
+        'creatinine_phosphokinase': creatinine_phosphokinase,
+        'diabetes': diabetes,
+        'ejection_fraction': ejection_fraction,
+        'high_blood_pressure': high_blood_pressure,
+        'platelets': platelets,
+        'serum_creatinine': serum_creatinine,
+        'serum_sodium': serum_sodium,
+        'sex': sex,
+        'smoking': smoking,
+        'time': time
+    }, index=[0])
+
+    # Machine learning model.
+    machine_learning_model = pickle.load(open(model_filename, 'rb'))
+    prediction = machine_learning_model.predict(prediction_data)[0]
+
+    return prediction
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,14 @@
+// Package imports.
+const schedule = require("node-schedule");
+
 // Module imports.
 const app = require("./app");
+const jobs = require("./jobs");
+
+// Schedule server jobs.
+jobs.forEach(job => {
+  schedule.scheduleJob(job.recurrenceRule, job.function);
+});
 
 // Server configuration.
 app.listen(3333);
